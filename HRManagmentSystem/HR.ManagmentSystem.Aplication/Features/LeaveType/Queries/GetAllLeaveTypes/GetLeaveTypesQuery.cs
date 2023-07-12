22ea86969cf9579f9a6744efb4f674b0bfd0271f
LbStoyanov <lbstoyanov1@gmail.com>
2023-07-12 22:49:45 +0300

    Creating LeaveTypes Handler

---
+++ HRManagmentSystem/HR.ManagmentSystem.Aplication/Features/LeaveType/Queries/GetAllLeaveTypes/GetLeaveTypesQuery.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.ManagmentSystem.Aplication.Features.LeaveType.Queries.GetAllLeaveTypes
+{
+    public class GetLeaveTypesQuery : IRequest<List<LeaveTypeDto>>
+    {
+    }
+
+    //Deference between class and record is that records are inmutable
+    //public record GetLeavesTypesQuery : IRequest<List<LeaveTypeDto>>;
+}
---
+++ HRManagmentSystem/HR.ManagmentSystem.Aplication/Features/LeaveType/Queries/GetAllLeaveTypes/GetLeaveTypesQueryHandler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.ManagmentSystem.Aplication.Features.LeaveType.Queries.GetAllLeaveTypes
+{
+    public class GetLeaveTypesQueryHandler : IRequestHandler<GetLeaveTypesQuery, List<LeaveTypeDto>>
+    {
+        public Task<List<LeaveTypeDto>> Handle(GetLeaveTypesQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
