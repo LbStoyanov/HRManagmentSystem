e1efc4cabd7bf6f6cef286b982de7f9edbeb5395
LbStoyanov <lbstoyanov1@gmail.com>
2023-07-12 22:08:27 +0300

    Creating The Repository Pattern

--- HRManagmentSystem/HR.ManagmentSystem.Aplication/Contracts/Persistence/IGenericRepository.cs
+++ HRManagmentSystem/HR.ManagmentSystem.Aplication/Contracts/Persistence/IGenericRepository.cs
@@ -1,7 +1,20 @@
-﻿namespace HR.ManagmentSystem.Aplication
-{
-    public class Class1
-    {
+﻿using HR.ManagmentSystem.Domain;
 
-    }
+namespace HR.ManagmentSystem.Aplication.Contracts.Persistence;
+
+public interface IGenericRepository<T> where T : class
+{
+    Task<T> GetAsync();
+
+    Task<T> GetByIdAsync(int id);
+    Task<T> CreateAsync(T entity);
+
+    Task<T> UpdateAsync(T entity);
+
+    Task<T> DeleteAsync(T entity);
+
+}
+public interface ILeaveTypeReository<T> : IGenericRepository<LeaveType>
+{
+
 }
