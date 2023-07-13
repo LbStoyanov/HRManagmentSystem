8b37d0694b5e7fc2777a3723f57f289a09c893dd
LbStoyanov <lbstoyanov1@gmail.com>
2023-07-13 18:52:38 +0300

    Adding Update and Delete Handlers

---
+++ HRManagmentSystem/HR.ManagmentSystem.Aplication/Features/LeaveType/Commands/DeleteLeaveType/DeleteLeaveTypeCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.ManagmentSystem.Aplication.Features.LeaveType.Commands.DeleteLeaveType
+{
+    internal class DeleteLeaveTypeCommandHandler
+    {
+    }
+}
