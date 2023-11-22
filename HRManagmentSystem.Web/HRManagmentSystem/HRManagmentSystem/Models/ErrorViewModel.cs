0063d4d8f42ed6d0d7ebf43ad54077bc711ffa36
LbStoyanov <lbstoyanov1@gmail.com>
2023-11-22 07:04:39 +0200

    InitialCreate

---
+++ HRManagmentSystem.Web/HRManagmentSystem/HRManagmentSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HRManagmentSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
