24b6f1946c18c92cee8965c53e51d16b2da93a7a
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2023-11-22 11:37:00 +0200

    Create BaseEntity.cs

---
+++ HRManagmentSystem.Web/HRManagmentSystem/HRManagmentSystem/Data/BaseEntity.cs
@@ -0,0 +1,12 @@
+namespace HRManagmentSystem.Data
+{
+    public partial class BaseEntity
+    {
+        public int Id { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public DateTime DateModified { get; set; }
+
+    }
+}
