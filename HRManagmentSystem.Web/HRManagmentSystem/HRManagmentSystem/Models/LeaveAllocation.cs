e1c137cd461ffd672ec8b75b19cc50b773e9a948
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2023-11-28 10:02:30 +0200

    Update LeaveAllocation.cs

--- HRManagmentSystem.Web/HRManagmentSystem/HRManagmentSystem/Models/LeaveAllocation.cs
+++ HRManagmentSystem.Web/HRManagmentSystem/HRManagmentSystem/Models/LeaveAllocation.cs
@@ -1,8 +1,24 @@
-﻿using HRManagmentSystem.Data;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace HRManagmentSystem.Models
+namespace HRManagmentSystem.Data
 {
     public class LeaveAllocation : BaseEntity
     {
+        public int Id { get; set; }
+
+        public int NumberOfDays { get; set; }
+
+
+        [ForeignKey("LeaveTypeId")]
+        public LeaveType LeaveType { get; set; }
+
+        public int LeaveTypeId { get; set; }
+
+
+        public string EmployeeId { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public DateTime DateModified { get; set; }
     }
 }
